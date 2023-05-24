0462c54c629de8f7eb4f0c1f583d7fdc55b57623
Maksym <makspat2000@gmail.com>
2023-05-24 22:50:20 +0200

    add .env

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
+const AdminId = process.env.Admin_Chat_Id;
+const token = process.env.Bot_token;
+
 const TelegramApi = require("node-telegram-bot-api");
-
-const token = "6252864986:AAEtgVwWhKNahWkioMu9EgQZFxxSdt81Rn8";
-
 const bot = new TelegramApi(token, {polling: true});
-
 
 const mainKeyboard = {
     "reply_markup": {
@@ -12,7 +12,12 @@ const mainKeyboard = {
     };
 
 bot.on("message", msg=>{
-    
+    console.log(isAdmin(msg.chat.id));
 
-    bot.sendMessage(msg.chat.id, msg.text, mainKeyboard);
+    bot.sendMessage(msg.chat.id, "Hello", mainKeyboard);
 });
+
+function isAdmin(chatId){
+    if(AdminId == chatId) return true;
+    else return false;
+}
